0eea8e453436e95247c2828f1f45157f852be224
Steve Scammon <stevescammon@me.com>
2022-05-03 13:38:33 -0400

    full gallery load switching

--- js/main.js
+++ js/main.js
@@ -1,28 +1,23 @@
-let currentPage = 'flowers'
-
 const $$ = document.querySelector.bind(document)
-
-// class Asset {
-//     constructor(name,image) {
-//         this.name = name
-//         this.image = image
-//     }
-
-// }
-
+let currentPage = 'flowers'
 let assets = {
     flowers: {
         count: 19,
+        bgcolor: 'white',
+        color: 'rgb(68, 95, 58)',
         images: []
     },
     glass: {
         count: 38,
+        bgcolor: 'black',
+        color: 'white',
         images: []
     }
 }
 const types = ['flowers','glass']
 
 types.forEach(type => {
+    document.querySelector(`#${type}`).addEventListener('click', changePage, type)
     for(i=1;i<=assets[type].count; i++) {
         assets[type].images.push(`./img/${type}/${i}.jpeg`)
     }
@@ -30,13 +25,23 @@ types.forEach(type => {
 
 console.table(assets)
 
-let galleryHTML = ""
+function changePage(type) {
+    console.log('You clicked me!')
+    let galleryHTML = ""
+    type = type.target.innerHTML.toLowerCase()
+    console.log(type)
+    $$('html').style.background = assets[type].bgcolor
+    $$('html').style.color = assets[type].color
+    $$('#logo').data = `./img/logo${type}.svg`
 
-assets[currentPage].images.forEach(image => {
-    galleryHTML += `<img src=${image}>`
-})
+    assets[type].images.forEach(image => {
+        galleryHTML += `<img src=${image}>`
+    })
+    
+    $$("#galleryMain").innerHTML = galleryHTML
+}
 
-$$("#galleryMain").innerHTML = galleryHTML
+
 
 
 
