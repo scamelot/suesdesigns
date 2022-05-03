c17ae05ce0e8f0d608cf3cb3b5c69fc465a9ece4
Steve Scammon <stevescammon@me.com>
2022-05-03 12:34:25 -0400

    full gallery load

--- js/main.js
+++ js/main.js
@@ -2,10 +2,42 @@ let currentPage = 'flowers'
 
 const $$ = document.querySelector.bind(document)
 
-let flowerImages = []
-let glassImages = []
+// class Asset {
+//     constructor(name,image) {
+//         this.name = name
+//         this.image = image
+//     }
 
-$.getJSON("./img/flowers", data => {
-    flowerImages = data
+// }
+
+let assets = {
+    flowers: {
+        count: 19,
+        images: []
+    },
+    glass: {
+        count: 38,
+        images: []
+    }
+}
+const types = ['flowers','glass']
+
+types.forEach(type => {
+    for(i=1;i<=assets[type].count; i++) {
+        assets[type].images.push(`./img/${type}/${i}.jpeg`)
+    }
 })
-console.log(flowerImages)
+
+console.table(assets)
+
+let galleryHTML = ""
+
+assets[currentPage].images.forEach(image => {
+    galleryHTML += `<img src=${image}>`
+})
+
+$$("#galleryMain").innerHTML = galleryHTML
+
+
+
+
